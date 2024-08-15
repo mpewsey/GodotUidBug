c94ae04ade2404962a2090fe081caa6b58987466
Matt Pewsey <23442063+mpewsey@users.noreply.github.com>
2024-08-15 09:20:00 -0400

    Initial commit

---
+++ addons/dummy/DummyPlugin.cs
@@ -0,0 +1,26 @@
+#if TOOLS
+using Godot;
+
+[Tool]
+public partial class DummyPlugin : EditorPlugin
+{
+    public override void _EnterTree()
+    {
+        var uidPath = "uid://dr1hds7vve78t";
+        var resPath = "res://addons/dummy/resource.tres";
+        var uidResource = ResourceLoader.Load<Resource>(uidPath);
+
+        if (uidResource == null)
+            GD.PrintErr("Resource is null, meaning it couldn't be loaded by uid:// path.");
+        else
+            GD.Print("Resource could be loaded by uid:// path.");
+
+        var resResource = ResourceLoader.Load<Resource>(resPath);
+
+        if (resResource == null)
+            GD.PrintErr("Resource is null, meaning it couldn't be loaded by res:// path.");
+        else
+            GD.Print("Resource could be loaded by res:// path.");
+    }
+}
+#endif
